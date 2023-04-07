52c05da90fb9a368b2ec9ea74e1d94efd8ee0fb7
Vishal <vpbgkt@gmail.com>
2023-04-08 01:03:44 +0530

    subject dynamic id added

--- Django/wscubetech/wscubetech/urls.py
+++ Django/wscubetech/wscubetech/urls.py
@@ -7,11 +7,11 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
+    1. Add an import:  from other_app.views ome
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))import H
 """
 from django.contrib import admin
 from django.urls import path
@@ -22,7 +22,10 @@ urlpatterns = [
     path('', views.homepage),
     path('aboutus/', views.aboutus, name='aboutus'),
     path('subject/', views.subject, name='subject'),
-    path('subject/tutorial/', views.tutorial)
+    path('subject/tutorial/', views.tutorial, name='subject'),
+    path('subject/<int:subject_id>/', views.subject_videos, name='subject_videos'),
+
+
 ]
 
 
--- Django/wscubetech/wscubetech/views.py
+++ Django/wscubetech/wscubetech/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from Subjects.models import Subjects
 from Video.models import Video
+from django.shortcuts import render, get_object_or_404
+
+
 def aboutus(request):
     return HttpResponse('Welcome to wscube tech')
 
@@ -29,3 +32,11 @@ def tutorial(request):
         'videoData':videoData
         }
     return render(request, 'pages/tutorial.html',Data)
+
+
+def subject_videos(request, subject_id):
+    subject = get_object_or_404(Subjects, pk=subject_id)
+    videos = Video.objects.filter(Subjects=subject)
+    context = {'subject': subject, 'videos': videos}
+    print(context,videos)
+    return render(request, 'pages/subject.html', context)
